d65ffba17dd2627a9d093d344ad2e99d5eb59090
Arrun21 <achoi200@caledonian.ac.uk>
2023-01-24 21:53:03 +0000

    add mobile responsive

--- src/Exercise.js
+++ src/Exercise.js
@@ -16,7 +16,7 @@ const Exercise = ({items}) => {
                 </header>
                 <p className='item-text'>{desc}</p>
                 <br></br>
-                <iframe width="410" height="315" src={vid} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <iframe id='video'width="410" height="315" src={vid} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
         </article>
     );
