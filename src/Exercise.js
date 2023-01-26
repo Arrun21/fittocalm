bf10885f4410b34dfee0ebaa0aa11602dc95fe8a
Arrun21 <achoi200@caledonian.ac.uk>
2023-01-26 18:58:54 +0000

    edit video css

--- src/Exercise.js
+++ src/Exercise.js
@@ -16,7 +16,7 @@ const Exercise = ({items}) => {
                 </header>
                 <p className='item-text'>{desc}</p>
                 <br></br>
-                <iframe id='video'width="410" height="315" src={vid} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <iframe id='video'src={vid} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
         </article>
     );
